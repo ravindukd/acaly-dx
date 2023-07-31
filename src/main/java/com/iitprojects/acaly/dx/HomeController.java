3189c36e08f9b6f70d3e3dbfa607ee08c10ff189
R.K. Dilshan <ravindukd@gmail.com>
2023-07-31 10:16:47 +0530

    UI Setup

--- src/main/java/com/iitprojects/acaly/dx/HomeController.java
+++ src/main/java/com/iitprojects/acaly/dx/HomeController.java
@@ -6,7 +6,14 @@ package com.iitprojects.acaly.dx;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -15,12 +22,29 @@ import javafx.fxml.Initializable;
  */
 public class HomeController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    VBox categoriesVbox;
+    
+    @FXML
+    private Accordion filtersAccord;
+    
+    @FXML
+    private TitledPane catPane;
+    @FXML
+    private TitledPane pricePane;
+
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        filtersAccord.setExpandedPane(catPane);
     }    
     
+    @FXML
+    private void addToCatVbox(){
+        Button catBtn = new Button("♒ All");
+        catBtn.setMaxWidth(Double.MAX_VALUE);
+        catBtn.setAlignment(Pos.TOP_LEFT);
+        catBtn.getStyleClass().add("catListItem");
+        categoriesVbox.getChildren().add(catBtn);
+    }
 }
